a3a0c7837f5c3fb731e27730b77411418de574e0
devganesh123 <86190761+devganesh123@users.noreply.github.com>
2022-10-22 18:14:28 +0530

    ✨request completed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const axios = require("axios").default;
-
 let seatItems = document.querySelectorAll(".seat-item");
-let confirmModal = document.querySelector("#confirm-booking");
+let modalContainer = document.querySelector("#modal-section");
 let closeBtn = document.querySelector("#close-btn");
 let form = document.querySelector("#seatbooking-form");
 let cancelBtn = document.querySelector("#cancel-btn");
 let cancelBookingBtn = document.querySelector("#cancel-booking");
+
+let message = document.querySelector("#message");
 
 seatItems.forEach((item) => {
   item.addEventListener("click", () => setModal(item));
@@ -13,16 +13,20 @@ seatItems.forEach((item) => {
 
 function setModal(seatItem) {
   // open the modal
-  confirmModal.classList.add("show");
+  modalContainer.classList.add("show");
 
   //  insert desk id
   let deskInput = document.querySelector("#desk-id");
   let deskValue = seatItem.textContent;
   deskInput.value = deskValue;
+
+  // form submit function
+  form.addEventListener("submit", (event) => {
+    formSubmit(event, seatItem);
+  });
 }
 
-// form submit function
-form.addEventListener("submit", (e) => {
+function formSubmit(e, seat) {
   e.preventDefault();
 
   const formData = new FormData(form);
@@ -33,45 +37,66 @@ form.addEventListener("submit", (e) => {
     console.log(item[0], item[1]);
   }
 
-  fetch("https://httpbin.org/post", {
-    method: "POST",
-    body: payload,
+  //   fetch("https://httpbin.org/post", {
+  //     method: "POST",
+  //     body: payload,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "color-code": "amber",
+  //     },
+  //   })
+  //.then((res) => res.json())
+  //   .then((output) => {
+  //     console.log(output.data.form);
+  //   })
+  //   .catch((error) => {
+  //     throw error;
+  //   });
+
+  axios({
+    method: "post",
+    url: "https://httpbin.org/post",
+    data: payload,
     headers: {
-      "Content-Type": "application/json",
       "color-code": "amber",
     },
   })
-    .then((res) => res.json())
     .then((output) => {
       console.log(output);
+      if (output.status === 200) {
+        seatBooking(seat);
+      }
     })
     .catch((error) => {
       throw error;
     });
-});
+}
 
 //close the modal
 closeBtn.addEventListener("click", closeModal);
 cancelBtn.addEventListener("click", closeModal);
 
 function closeModal() {
-  confirmModal.classList.remove("show");
+  modalContainer.classList.remove("show");
 }
 
-function seatBooking(e) {
-  // confirmModal.classList.remove("show");
-  // let childrens = [...seat.parentElement.children];
-  // console.log(childrens);
-  // childrens.forEach((children) => {
-  //   children.classList.remove("booked");
-  // });
-  // seat.classList.add("booked");
-  // console.log(seat);
+function seatBooking(seat) {
+  // close the modal when the response is sent
+  modalContainer.classList.remove("show");
+
+  // changing the seat status to booked
+  let childrens = [...seat.parentElement.children];
+  console.log(childrens);
+  childrens.forEach((children) => {
+    children.classList.remove("booked");
+  });
+  seat.classList.add("booked");
+  console.log(seat);
 }
 
-// cancelBookingBtn.addEventListener("click", (item) => cancelBooking(item));
+cancelBookingBtn.addEventListener("click", (item) => cancelBooking(item));
 
-// function cancelBooking(seat) {
-//   let bookedSlot = document.querySelector(".seat-item.booked");
-//   bookedSlot.classList.remove("booked");
-// }
+function cancelBooking(seat) {
+  let bookedSlot = document.querySelector(".seat-item.booked");
+  bookedSlot.classList.remove("booked");
+}
