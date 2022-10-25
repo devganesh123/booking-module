21ee04ceb09543c2076c663353cfe890971257b4
devganesh123 <86190761+devganesh123@users.noreply.github.com>
2022-10-25 22:49:23 +0530

    ✨post request using async awati

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// const { default: axios } = require("axios");
+
 let seatItems = document.querySelectorAll(".seat-item");
 let modalContainer = document.querySelector("#modal-section");
 let closeBtn = document.querySelector("#close-btn");
 let form = document.querySelector("#seatbooking-form");
 let cancelBtn = document.querySelector("#cancel-btn");
 let cancelBookingBtn = document.querySelector("#cancel-booking");
+const overlay = document.getElementById("overlay");
 
 let message = document.querySelector("#message");
 
@@ -14,6 +17,7 @@ seatItems.forEach((item) => {
 function setModal(seatItem) {
   // open the modal
   modalContainer.classList.add("show");
+  overlay.classList.add("active");
 
   //  insert desk id
   let deskInput = document.querySelector("#desk-id");
@@ -33,6 +37,7 @@ function formSubmit(e, seat) {
   const result = Object.fromEntries(formData);
   const payload = JSON.stringify(result);
 
+  console.log(payload);
   for (item of formData) {
     console.log(item[0], item[1]);
   }
@@ -53,23 +58,54 @@ function formSubmit(e, seat) {
   //     throw error;
   //   });
 
-  axios({
-    method: "post",
-    url: "https://httpbin.org/post",
-    data: payload,
-    headers: {
-      "color-code": "amber",
-    },
-  })
-    .then((output) => {
-      console.log(output);
-      if (output.status === 200) {
-        seatBooking(seat);
-      }
-    })
-    .catch((error) => {
-      throw error;
-    });
+  // axios({
+  //   method: "post",
+  //   url: "https://httpbin.org/post",
+  //   data: payload,
+  //   headers: {
+  //     "color-code": "amber",
+  //   },
+  // })
+  //   .then((output) => {
+  //     console.log(output);
+  //     if (output.status === 200) {
+  //       seatBooking(seat);
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     throw error;
+  //   });
+  const url = "https://reqres.in/api/users";
+  const fetchData = async () => {
+    try {
+      const response = await axios(url, {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      response.data.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(fetchData());
+
+  const postUrl = "https://reqres.in/api/users";
+  const postData = async () => {
+    try {
+      const response = await axios.post(postUrl, {
+        data: payload,
+        headers: {
+          "Content-Type": "application/json",
+          "booked-status": "booked",
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(postData());
 }
 
 //close the modal
@@ -78,6 +114,7 @@ cancelBtn.addEventListener("click", closeModal);
 
 function closeModal() {
   modalContainer.classList.remove("show");
+  overlay.classList.remove("active");
 }
 
 function seatBooking(seat) {
@@ -85,6 +122,7 @@ function seatBooking(seat) {
   form.reset();
   // close the modal when the response is sent
   modalContainer.classList.remove("show");
+  overlay.classList.remove("active");
 
   seatItems.forEach((item) => {
     item.classList.remove("booked");
@@ -92,9 +130,9 @@ function seatBooking(seat) {
   seat.classList.add("booked");
 }
 
-cancelBookingBtn.addEventListener("click", (item) => cancelBooking(item));
+// cancelBookingBtn.addEventListener("click", (item) => cancelBooking(item));
 
-function cancelBooking(seat) {
-  let bookedSlot = document.querySelector(".seat-item.booked");
-  bookedSlot.classList.remove("booked");
-}
+// function cancelBooking(seat) {
+//   let bookedSlot = document.querySelector(".seat-item.booked");
+//   bookedSlot.classList.remove("booked");
+// }
